6172d94e95370c0d5703e21d1e86344e3fa88b80
Steven Vance <steve@stevevance.net>
2013-02-22 14:32:51 -0600

    Removed whitespace

--- index.php
+++ index.php
@@ -262,7 +262,6 @@ function getUrl() {
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
 </script>
 </body>
 </html>
