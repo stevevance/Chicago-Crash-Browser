00bf75e21db84d98151fb346112c1b1519488d9f
Steven Vance <steve@stevevance.net>
2014-03-18 21:40:02 -0500

    New map tiles

--- js/crashbrowser.js
+++ js/crashbrowser.js
@@ -91,7 +91,7 @@ var mapDisplay = (function() {
         map.addControl(new L.Control.Permalink({useLocation:true}));
         map.addControl(new L.control.locate({debug:false}));
         // add an OpenStreetMap tile layer
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        L.tileLayer('https://{s}.tiles.mapbox.com/v3/foursquare.m3elv7vi/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
             maxZoom: 19
         }).addTo(map);
