f62c49408692a5d43a30800a127428cd462c30a4
Robert Guico <rguico@gmail.com>
2015-03-12 15:50:41 -0500

    Simply api.php

--- api.php
+++ api.php
@@ -1,5 +1,4 @@
 <?php
-//echo "test";
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
@@ -33,128 +32,30 @@ if($distance > 500) { // in feet
 	$distance = 150;
 }
 
-$distanceMiles = distanceCheck($distance);
+// constant for ST_Transform
+$NAD83_ILLINOIS_EAST = 3435;
+$WGS_84 = 4326;
 
-$carsToo = false;
-	if($_GET['carsToo'] == 1) {
-		$carsToo = true;
-	}
-	if($carsToo == false) {
-		$addThis = 'AND(p."PersonType" = 3 OR p."PersonType" = 2)';
-	}
-
-$sql3 = 'SELECT
-	min(C ."noInjuries") + min(C ."totalInjuries") people,
-	min(C ."totalKilled") fatalities,
-	min(C ."Crash severity") severity,
-	C .casenumber,
-	min(C ."totalInjuries") injuries,
-	min(C ."noInjuries") noInjuries,
-	min(C ."Crash injury severity"),
-	min(C ."totalKilled") totalKilled,
-	min(C .year) cyear,
-	min(C .month) cmonth,
-	min(C .day) cday,
-	min(C ."Crash latitude") latitude,
-	min(C ."Crash longitude") longitude,
-	MIN (P ."PersonType") persontype,
-	min(c."collType") collisiontype
-FROM
-	"'.$tableCrashes.'" c JOIN
-	"'.$tablePersons.'" p USING (casenumber)
+$sql = <<< HEREDOC
+SELECT array_to_json(array_agg(row_to_json(t))) AS result FROM (
+SELECT "collType", casenumber, "totalInjuries", "Total killed" as "totalKilled", "No injuries" as "noInjuries",
+	month, day, year, latitude, longitude FROM "$table" c
 WHERE
-	c."City Code" = 1051
-AND	c."Crash latitude" < '.$north.' AND c."Crash latitude" > '.$south.'
-AND c."Crash longitude" > '.$west.' AND c."Crash longitude" < '.$east.'
- '. $addThis .'
-AND ST_DWithin((SELECT ST_Transform(ST_GeomFromText(\'POINT('.$lng.' '.$lat.')\',4326),3436)), ST_Transform(wgs84, 3436), '.$distance.')
-GROUP BY c.casenumber
-ORDER BY
-	MIN (C ."year"),
-	MIN (C ."month"),
-	MIN (C ."day")';
-
-$sql4 = 'SELECT * FROM "'.$table.'" c
-WHERE
-ST_DWithin((SELECT ST_Transform(ST_GeomFromText(\'POINT('.$lng.' '.$lat.')\',4326),3436)), ST_Transform(c.wgs84, 3436), '.$distance.')
-AND "Crash latitude"::decimal != 0
-AND "Crash longitude"::decimal != 0
-AND	c."Crash latitude"::decimal < '.$north.' AND c."Crash latitude"::decimal > '.$south.'
-AND c."Crash longitude"::decimal > '.$west.' AND c."Crash longitude"::decimal < '.$east.'
-ORDER BY c.month ASC, c.day ASC';
-
-$sql5 = 'SELECT * FROM "'.$table.'" c
-WHERE
-ST_DWithin((SELECT ST_GeomFromText(\'POINT('.$lng.' '.$lat.')\',4326)), c.wgs84), '.$distance.')
-AND latitude::decimal != 0
-AND longitude::decimal != 0
-AND	latitude::decimal < '.$north.' AND latitude::decimal > '.$south.'
-AND longitude::decimal > '.$west.' AND longitude::decimal < '.$east.'
-ORDER BY c.month ASC, c.day ASC';
-
-$sql6 = 'SELECT * FROM "'.$table.'" c
-WHERE
-ST_DWithin((SELECT ST_Transform(ST_GeomFromText(\'POINT('.$lng.' '.$lat.')\',4326),3435)), ST_Transform(c.wgs84, 3435), '.$distance.')
-AND	latitude < '.$north.' AND latitude > '.$south.'
-AND longitude > '.$west.' AND longitude < '.$east.'
-ORDER BY c.month ASC, c.day ASC';
-
-$sql7 = '
-SELECT array_to_json(array_agg(row_to_json(t))) as result from (
-SELECT "collType", casenumber, "totalInjuries", "Total killed" as "totalKilled", "No injuries" as "noInjuries", month, day, year, latitude, longitude FROM "'.$table.'" c
-WHERE
-ST_DWithin((SELECT ST_Transform(ST_GeomFromText(\'POINT('.$lng.' '.$lat.')\',4326),3435)), ST_Transform(c.wgs84, 3435), '.$distance.')
-AND latitude < '.$north.' AND latitude > '.$south.'
-AND longitude > '.$west.' AND longitude < '.$east.'
+ST_DWithin((SELECT ST_Transform(ST_GeomFromText('POINT( $lng $lat )',$WGS_84),$NAD83_ILLINOIS_EAST)), ST_Transform(c.wgs84, $NAD83_ILLINOIS_EAST), $distance)
+AND latitude < $north AND latitude > $south
+AND longitude > $west AND longitude < $east
 ORDER BY year ASC, month ASC, day ASC
-) as t';
+) as t
+HEREDOC;
 
-$sql8 = 'SELECT
-	"collType",
-	casenumber,
-	"totalInjuries",
-	"Total killed" AS "totalKilled",
-	"No injuries" AS "noInjuries",
-	MONTH,
-	DAY,
-	YEAR,
-	latitude,
-	longitude
-FROM
-	"'.$table.'" C
-WHERE
-	ST_DWithin (
-		(
-
-				ST_Transform (
-					ST_GeomFromText (
-						\'POINT('.$lng.' '.$lat.')\',
-						4326
-					),
-					3435
-				)
-		),
-		geom_3435,
-		150
-	)
-ORDER BY
-	YEAR ASC,
-	MONTH ASC,
-	DAY ASC';
-
-// ERROR:  transform: couldn't project point (0 0 0): latitude or longitude exceeded limits (-14)
-
-//echo $choice;
 if(!empty($lat) && !empty($lng)) {
-	$result = pg_query($pg, $sql7);
+	$result = pg_query($pg, $sql);
 	$total = pg_num_rows($result);
 }
-//echo "<p>".$sql4."</p>";
 
 echo pg_last_error($pg);
 
 $r = pg_fetch_assoc($result);
 $result = $r['result'] ? $r['result'] : '[]';
-// output JSON
-echo '{"response":{"sql":' . json_encode($sql7) . '},"crashes":' . $result . '}';
+echo '{"response":{"sql":' . json_encode($sql) . '},"crashes":' . $result . '}';
 ?>
