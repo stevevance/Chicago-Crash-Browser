80b373fdf14a5698490eafed01e6f728073602aa
Steven Vance <steve@stevevance.net>
2016-03-25 22:23:53 -0500

    create a config file

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+// DEFINE constants
+$config = [
+	"srid_wgs84"=>4326, 
+	"srid_illinois_east"=>3435, 
+	"table_crashes"=>"crashes_chicago_2009_2014"
+];
+
+foreach($config as $key=>$val):
+	define(strtoupper($key),$val);
+endforeach;
+
+// set the path so we can find config.php no matter where this cron file is stored
+$path = "/";
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+$path = "../";
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+?>x
